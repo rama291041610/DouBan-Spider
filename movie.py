0c115bd5867ba6814fea73873c7faf2c15c3b025
惊蛰 <jingzhe@live.cn>
2018-06-08 13:30:36 +0800

    Update movie.py

--- movie.py
+++ movie.py
@@ -3,9 +3,11 @@
 from text import *
 import requests
 import datetime
+import threading
 import time
 import json
 import bs4
+import os
 import re
 
 headers = {
@@ -94,6 +96,44 @@ class Movie(object):
                     role_list.append({'name': name, 'role': role})
         return {'type': genres, 'runtime': runtime, 'country': country.group(1), 'score': score, 'ReleaseDate': ReleaseDate, 'director': director, 'role_list': role_list}
 
+    def save_img(self):
+        if os.path.exists('./img') == False:
+            os.mkdir('./img')
+        if self.douban_id == None:
+            self.set_douban_id()
+        r = requests.get('https://api.douban.com/v2/movie/subject/' +
+                         str(self.douban_id) + '?apikey=0b2bdeda43b5688921839c8ecb20399b')
+        img_url = r.json().get('images', None).get('large', None)
+        img_content = requests.get(img_url, headers=headers)
+        if img_url and 'jpg' in img_url:
+            f = open('./img/' + self.name + '.jpg', 'wb')
+            f.write(img_content.content)
+            f.close()
+            order = 'cwebp "./img/%s" -o "./img/%s"' % (self.name + '.jpg', self.name + '.webp')
+            os.system(order)
+            os.remove('./img/' + self.name + '.jpg')
+            order = 'dwebp "./img/%s" -o "./img/%s"' % (self.name + '.webp', self.name + '.png')
+            os.system(order)
+            os.remove('./img/' + self.name + '.webp')
+            return True
+        elif img_url and '.webp' in img_url:
+            f = open('./img/' + self.name + '.webp', 'wb')
+            f.write(img_content.content)
+            f.close()
+            order = 'dwebp "./img/%s" -o "./img/%s"' % (self.name + '.webp', self.name + '.png')
+            os.system(order)
+            os.remove('./img/' + self.name + '.webp')
+            return True
+        elif img_url and '.png' in img_url:
+            f = open('./img/' + self.name + '.webp', 'wb')
+            f.write(img_content.content)
+            f.close()
+            return True
+        else:
+            print("WTF!!!")
+            print(img_url)
+            os._exit(0)
+
     def get_booking(self):
         if self.maoyan_id == None:
             self.set_maoyan_id()
@@ -142,9 +182,9 @@ def get_film_top250():
     return film_list
 
 
-def get_film_list_fm(p):
-    r = requests.get('http://dianying.fm/search/?p=' + str(p), cookies={
-                     'sessionid': '".eJxVy80OgjAQBOB36VlISyi03vDkyQsP0Gy325SIkPTHxBjfXTBeuE1m5nszNyVcnxRfBkuMtGR2ZjOl6jGlKoLdo9BSsRMzUHIwJVE0AVLYftpqtMilcui096LvWoVKcuebnnMgJbXzXtERW8A7LW7zgLiWJde_LQUQ9W0dYc7jdRDD1l3-zwOfdtk2otOSfb6S2kA_:1fC5qU:6HD585xd0sOeZUNvM2CgtGrAnSQ"'})
+def get_film_list_fm(p, year=2018):
+    r = requests.get('http://dianying.fm/search/?sort=douban_rank&year=' + str(year) + '&p=' + str(p), cookies={
+                     'sessionid': '".eJxVy7EOgjAQxvF3udmQllC4uuHk5MIDNNdrLzUaSGg7Gd8dMA66fv_v9wJHtSRXc1xdopzgDNZb9qwMBg5WRA99h4xGBWkHpSiisUEEI5x-sSd-xDnsnpiXOpfm03Ii3dyWiZ5luo563LfL9_nH74fsWt1bA-8NtNYxsw:1fOe9C:FEs-p-yZ_5qFQbmT0t-alOhKmYM"'})
     film = bs4.BeautifulSoup(r.text, "lxml").find_all(attrs={'class': 'fm-movie-title'})
     film_list = []
     for x in film:
@@ -156,3 +196,13 @@ def get_film_maoyan(id):
     r = requests.get('http://maoyan.com/films/' + str(id) + '?_v_=yes', headers=headers)
     soup = bs4.BeautifulSoup(r.text, "lxml")
     return soup.h3.string
+
+
+if __name__ == '__main__':
+    with open('film.txt', 'r', encoding='utf-8') as f:
+        film_list = f.readlines()
+        f.close()
+    for x in film_list:
+        m = Movie(x.strip())
+        m.save_img()
+    print("FINISH.")
